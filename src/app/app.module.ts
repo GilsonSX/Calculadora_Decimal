b3c3a0fbc8c47cd28178a09d01d247d219791755
Jessica Barbosa <lbarbosa.jessica@gmail.com>
2020-09-26 10:35:42 -0300

    aula dia 26/09 - services e modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TecladoComponent } from './teclado/teclado.component';
-//import { AlertModule } from 'ngx-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TecladoComponent } from "./teclado/teclado.component";
+import { FormsModule } from "@angular/forms";
+import { InformacoesModule } from "./informacoes/informacoes.module";
 
 @NgModule({
   declarations: [AppComponent, TecladoComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    //AlertModule.forRoot(),
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, InformacoesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/teclado/teclado.component.ts
+++ src/app/teclado/teclado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ValidacaoService } from "../validacao.service";
 
 @Component({
   selector: "app-teclado",
@@ -8,78 +9,12 @@ import { Component, OnInit } from "@angular/core";
 export class TecladoComponent implements OnInit {
   public resultado = "";
 
-  constructor() {}
+  constructor(public validacao: ValidacaoService) {}
 
   ngOnInit() {}
 
-  //verifica se o caractere passado é um operador
-  public operador(caractere: string): boolean {
-    if (
-      caractere == "+" ||
-      caractere == "-" ||
-      caractere == "/" ||
-      caractere == "*"
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //verifica se o caractere passado é zero
-  public zero(caractere: string): boolean {
-    if (caractere == "0") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //verifica se o último caractere da expressão é um operador
-  //e o caractere passado é zero
-  public zeroOperador(caractere: string, expressao: string): boolean {
-    if (
-      this.operador(expressao.charAt(expressao.length - 1)) &&
-      this.zero(caractere)
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //verifica se o último caractere da expressão é um operador
-  //e o caractere passado é operador
-  public operadorOperador(caractere: string, expressao: string): boolean {
-    if (
-      this.operador(expressao.charAt(expressao.length - 1)) &&
-      this.operador(caractere)
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //verifica se a expressão é valida
-  public validaExpressao(caractere: string, expressao: string): boolean {
-    let valido: boolean = true;
-    if (expressao.length == 0) {
-      if (this.operador(caractere) || this.zero(caractere)) {
-        valido = false;
-      }
-    } else {
-      if (this.zeroOperador(caractere, expressao)) {
-        valido = false;
-      } else if (this.operadorOperador(caractere, expressao)) {
-        valido = false;
-      }
-    }
-    return valido;
-  }
-
   public montarResultado(digito: string): void {
-    if (this.validaExpressao(digito, this.resultado)) {
+    if (this.validacao.validaExpressao(digito, this.resultado)) {
       this.resultado += digito;
     }
   }
