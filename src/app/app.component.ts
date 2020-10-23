18285c952703707fea8a78bf72e79958fdcd575f
Jesse <lbarbosa.jessica@gmail.com>
2020-10-23 13:59:48 -0300

    input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora';
+  public criadoPor:string = 'Jessica Barbosa';
+  public ano:string = '2020';
 }
--- src/app/informacoes/sobre/sobre.component.ts
+++ src/app/informacoes/sobre/sobre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent implements OnInit {
+
+  @Input() author:string = "";
+  @Input() copyright:string = "";
 
   constructor() { }
 
